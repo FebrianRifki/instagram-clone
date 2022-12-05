654a559365e3f549c21d915a4c5f2e737aa5f8c5
Febrianrifki <febrianrifki1702@gmail.com>
2022-12-05 21:01:49 +0700

    state management

--- instagram_clone/lib/resources/auth_methods.dart
+++ instagram_clone/lib/resources/auth_methods.dart
@@ -2,11 +2,21 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/models/user.dart' as model;
 import 'package:instagram_clone/resources/storage_methods.dart';
 
 class AuthMethods {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future<model.User> getUserDetails() async {
+    User currentUser = _auth.currentUser!;
+
+    DocumentSnapshot snap =
+        await _firestore.collection('user').doc(currentUser.uid).get();
+
+    return model.User.fromSnap(snap);
+  }
 
   // sing up user
   Future<String> singUpUser({
@@ -31,15 +41,20 @@ class AuthMethods {
             .uploadImageToFirebase('profilePics', file, false);
 
         // add user to our database
-        await _firestore.collection('user').doc(cred.user!.uid).set({
-          'username': username,
-          'uid': cred.user!.uid,
-          'email': email,
-          'bio': bio,
-          'followers': [],
-          'following': [],
-          'photoUrl': photoUrl
-        });
+
+        model.User user = model.User(
+            username: username,
+            uid: cred.user!.uid,
+            email: email,
+            bio: bio,
+            photoUrl: photoUrl,
+            following: [],
+            followers: []);
+
+        await _firestore
+            .collection('user')
+            .doc(cred.user!.uid)
+            .set(user.toJson());
         res = 'success';
       }
     } on FirebaseAuthException catch (err) {
--- instagram_clone/lib/responsive/mobile_screen_layout.dart
+++ instagram_clone/lib/responsive/mobile_screen_layout.dart
@@ -1,14 +1,41 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/models/user.dart' as model;
+import 'package:instagram_clone/providers/user_provider.dart';
+import 'package:instagram_clone/utils/colors.dart';
+import 'package:provider/provider.dart';
 
-class MobileScreenLayout extends StatelessWidget {
+class MobileScreenLayout extends StatefulWidget {
   const MobileScreenLayout({Key? key}) : super(key: key);
 
   @override
+  State<MobileScreenLayout> createState() => _MobileScreenLayoutState();
+}
+
+class _MobileScreenLayoutState extends State<MobileScreenLayout> {
+  String username = "";
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('this is mobile screen'),
-      ),
-    );
+    model.User? user = Provider.of<UserProvider>(context).getuser;
+
+    return user == null
+        ? const Center(
+            child: CircularProgressIndicator(
+              color: primaryColor,
+            ),
+          )
+        : Scaffold(
+            body: Center(
+              child: Text(user.username),
+            ),
+          );
   }
 }
